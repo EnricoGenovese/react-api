fd7fe57aca41012719b5add6f8751a59b69d3332
EnricoGenovese <enricogeno@libero.it>
2025-01-07 16:25:28 +0100

    new commit after repo delete, able to stage only the front-end part;

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>My Posts</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+
+const apiUrl = "http://localhost:3000/posts";
+
+export default function Main({ removePost }) {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        // console.log("Effect used")
+        getData()
+    }, [])
+
+    function getData() {
+        axios.get(apiUrl).then((res) => {
+            console.log(res.data)
+            setPosts(res.data.data)
+        })
+    }
+
+    function removePost(id) {
+        axios.delete(apiUrl + "/" + id)
+        setPosts(posts.filter((post) => post.id !== id))
+    }
+    return (
+        <ul>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <h4>{post.title}</h4>
+                    <p>{post.content}</p>
+                    <img src={post.img} alt={post.title} />
+                    <p>{post.tags.join(", ")}</p>
+
+                    <button key={post.id}
+                        onClick={() => removePost(post.id)}
+                        className="btn btn-delete">
+                        Delete
+                    </button>
+                </li>
+
+            ))}
+
+        </ul>
+    )
+}
